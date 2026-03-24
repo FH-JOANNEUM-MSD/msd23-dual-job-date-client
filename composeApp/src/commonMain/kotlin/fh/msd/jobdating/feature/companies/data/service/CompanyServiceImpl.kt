6c16ac828a791de035d1d9f313605b9f4c608619
Alexander Winkler <alexander.winkler3@edu.fh-joanneum.at>
2026-03-24 13:58:20 +0100

    uadded voting api

--- composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/companies/data/service/CompanyServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/companies/data/service/CompanyServiceImpl.kt
@@ -12,6 +12,7 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
