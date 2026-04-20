9760b328fc25420c41fd8f0261a46ee43943309a
Headline42 <headline628@gmail.com>
2026-04-20 14:32:01 +0200

    updated the version number

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -104,7 +104,7 @@ android {
         applicationId = "fh.msd"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
+        versionCode = 13
         versionName = "1.0"
     }
     packaging {
