06b8916b5d19879eb0852197b3beb4b4b0a12a63
Headline42 <jonas.sieff@griehser.at>
2026-05-28 09:18:33 +0200

    fixed an issue with the logo

--- composeApp/src/commonMain/kotlin/fh/msd/jobdating/core/ui/AppBackground.kt
+++ composeApp/src/commonMain/kotlin/fh/msd/jobdating/core/ui/AppBackground.kt
@@ -3,13 +3,22 @@ package fh.msd.jobdating.core.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import dualjobdating.composeapp.generated.resources.Res
 import dualjobdating.composeapp.generated.resources.background
 import dualjobdating.composeapp.generated.resources.background_light
+import dualjobdating.composeapp.generated.resources.logo_light_txt
+import dualjobdating.composeapp.generated.resources.logo_txt
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -17,10 +26,16 @@ fun AppBackground(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    val bgPainter = if (isSystemInDarkTheme()) {
+    val isDark = isSystemInDarkTheme()
+    val bgPainter = if (isDark) {
         painterResource(Res.drawable.background)
     } else {
         painterResource(Res.drawable.background_light)
+    }
+    val logoPainter = if (isDark) {
+        painterResource(Res.drawable.logo_txt)
+    } else {
+        painterResource(Res.drawable.logo_light_txt)
     }
 
     Box(modifier = modifier.fillMaxSize()) {
@@ -29,6 +44,16 @@ fun AppBackground(
             contentDescription = null,
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.fillMaxSize()
+        )
+        Image(
+            painter = logoPainter,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 16.dp)
+                .windowInsetsPadding(WindowInsets.statusBars)
+                .width(160.dp)
         )
         content()
     }
--- composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/auth/ui/LoginState.kt
+++ composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/auth/ui/LoginState.kt
@@ -2,7 +2,7 @@
 package fh.msd.jobdating.feature.auth.ui
 
 data class LoginState(
-    val email: String = "arya.stark@westeros.com",
+    val email: String = "robb.stark@westeros.com",
     val password: String = "WinterIsComing123!",
     val isLoading: Boolean = false,
     val error: String? = null
