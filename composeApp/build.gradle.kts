d3163fed8bfef51dfb992aca9a4fc67111075aa3
Headline42 <headline628@gmail.com>
2026-05-30 12:00:50 +0200

    upped the version code trigger deploy

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -106,7 +106,7 @@ android {
         applicationId = "fh.msd"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 14
+        versionCode = 15
         versionName = "1.0"
     }
     packaging {
