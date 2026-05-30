5d826d27a0573b519a5a23ac4d44ce01e8908e1c
Headline42 <headline628@gmail.com>
2026-05-30 09:40:32 +0200

    removed logs from all production builds

--- composeApp/src/commonMain/kotlin/fh/msd/jobdating/core/network/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/fh/msd/jobdating/core/network/HttpClientFactory.kt
@@ -1,8 +1,12 @@
 package fh.msd.jobdating.core.network
 
+import fh.msd.jobdating.core.AppConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.cookies.HttpCookies
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -17,6 +21,13 @@ object HttpClientFactory {
                 ignoreUnknownKeys = true
                 isLenient = true
             })
+        }
+
+        if (!AppConfig.isProduction) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
         }
     }
 }
--- composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/auth/ui/LoginState.kt
+++ composeApp/src/commonMain/kotlin/fh/msd/jobdating/feature/auth/ui/LoginState.kt
@@ -1,9 +1,11 @@
 
 package fh.msd.jobdating.feature.auth.ui
 
+import fh.msd.jobdating.core.AppConfig
+
 data class LoginState(
-    val email: String = "robb.stark@westeros.com",
-    val password: String = "WinterIsComing123!",
+    val email: String = if (AppConfig.isProduction) "" else "robb.stark@westeros.com",
+    val password: String = if (AppConfig.isProduction) "" else "WinterIsComing123!",
     val isLoading: Boolean = false,
     val error: String? = null
 )
