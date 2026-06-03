055bbebb685e3e82321462945b4b614f7dc7831f
Headline42 <jonas.sieff@griehser.at>
2026-06-03 10:13:52 +0200

    ++ the version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -107,7 +107,7 @@ android {
         applicationId = "fh.msd"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 18
+        versionCode = 19
         versionName = "1.0"
     }
     packaging {
